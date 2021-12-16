e2b851ee564ba7afb830061a08ae1d4c3060373d
olaitan1978 <seeyiison@yahoo.com>
2021-12-16 11:20:02 +0100

    addtional link added to the search engine

--- Desktop/SEARCH/search.js
+++ Desktop/SEARCH/search.js
@@ -1,11 +1,17 @@
 let y= document.getElementById("solo")
 function bola(){
     let x= document.getElementById("sear").value
-    if(x==='animal'){
+    if(x==='animal kingdom'){
         window.open("animal.html")
         
     }else if(x=='seyi'){
         window.open("seyi.html")
+    }else if(x==='google'){
+        window.open('https://www.google.com.ng/')
+    }else if(x==='greenlife'){
+        window.open('https://www.greenlifepharmaceuticals.com/')
+    }else if(x==='bill gate'){
+        window.open('https://www.forbes.com/profile/bill-gates/?sh=41187b1e689f')
     }
 
 }
